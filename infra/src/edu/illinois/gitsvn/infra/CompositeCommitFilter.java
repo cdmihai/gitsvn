b8be15db9cf57401f988032fd5eaced9c2846203
Caius Brindescu <caius.brindescu@gmail.com>
2012-10-15 11:53:44 -0500

    Stub for our Composite Filter
    This filter should allow us to call multiple filters at once during the
    RevWalk.
    This would make the thing faster and more scalable.

---
+++ infra/src/edu/illinois/gitsvn/infra/CompositeCommitFilter.java
@@ -0,0 +1,22 @@
+package edu.illinois.gitsvn.infra;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.StopWalkException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.gitective.core.filter.commit.CommitFilter;
+
+public class CompositeCommitFilter extends CommitFilter {
+
+	@Override
+	public boolean include(RevWalk walker, RevCommit cmit)
+			throws StopWalkException, MissingObjectException,
+			IncorrectObjectTypeException, IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
