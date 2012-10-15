1d9bd6ac2581a7a8a9f657b4c822702825e5895c
Caius Brindescu <caius.brindescu@gmail.com>
2012-10-15 12:03:14 -0500

    Added JavaDoc

--- infra/src/edu/illinois/gitsvn/infra/CompositeCommitFilter.java
+++ infra/src/edu/illinois/gitsvn/infra/CompositeCommitFilter.java
@@ -11,14 +11,33 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.gitective.core.filter.commit.CommitFilter;
 
+/**
+ * Composite Filter that allows us to call multiple filters in one walk through
+ * the repository. This should make the whole thing more scalable and faster.
+ * 
+ * @author caius
+ * 
+ */
 public class CompositeCommitFilter extends CommitFilter {
 
 	private List<CommitFilter> filters = new ArrayList<CommitFilter>();
 
+	/**
+	 * Adds a filter to the list.
+	 * 
+	 * @param filter
+	 *            the filter to be added
+	 */
 	public void addFilter(CommitFilter filter) {
 		filters.add(filter);
 	}
 
+	/**
+	 * Removes a filter from the list.
+	 * 
+	 * @param filter
+	 *            the filter to be removed
+	 */
 	public void removeFilter(CommitFilter filter) {
 		filters.remove(filter);
 	}
@@ -27,7 +46,6 @@ public class CompositeCommitFilter extends CommitFilter {
 	public boolean include(RevWalk walker, RevCommit cmit)
 			throws StopWalkException, MissingObjectException,
 			IncorrectObjectTypeException, IOException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
