b8abb1c583b4b613015c2f594fe7a4690f134755
Caius Brindescu <caius.brindescu@gmail.com>
2012-10-15 15:16:00 -0500

    Implemented the RefactoringCrawler
    Implements the crawler. It goes over the repositories, and applies the
    filters.
    This way, we keep all this complicated thing hidden behind a nice
    interface.
    
    It also has a simple test.

---
+++ infra/src/edu/illinois/gitsvn/infra/RepositoryCrawler.java
@@ -0,0 +1,69 @@
+package edu.illinois.gitsvn.infra;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.gitective.core.CommitFinder;
+import org.gitective.core.filter.commit.CommitFilter;
+
+public class RepositoryCrawler {
+	
+	private List<Class<? extends CommitFilter>> filterClasses;
+	
+	public RepositoryCrawler() {
+		filterClasses = new ArrayList<Class<? extends CommitFilter>>();
+	}
+	
+	public void addFilterClass(Class<? extends CommitFilter> filterClass) {
+		filterClasses.add(filterClass);
+	}
+	
+	public void removeFilterClass(Class<? extends CommitFilter> filterClass) {
+		filterClasses.remove(filterClass);
+	}
+	
+	public List<CommitFilter> crawlRepo(String repoLoc) throws GitAPIException,
+			InvalidRemoteException, TransportException {
+		
+		String cloneDirName = "repos/clone" + System.nanoTime();
+		File cloneDir = new File(cloneDirName);
+		cloneDir.deleteOnExit();
+		Git repo = Git.cloneRepository().setURI(repoLoc)
+				.setDirectory(cloneDir).call();
+		
+		CommitFinder finder = new CommitFinder(repo.getRepository());
+		
+		CompositeCommitFilter filter = null;
+		try {
+			filter = createFilter();
+		} catch (InstantiationException | IllegalAccessException e) {
+			System.out.println("Error creating filter:" + e.getMessage());
+		}
+		finder.setFilter(filter);
+		finder.find();
+		
+
+		return filter.getFilters();
+	}
+
+	private CompositeCommitFilter createFilter() throws InstantiationException, IllegalAccessException {
+		CompositeCommitFilter compositeFilter = new CompositeCommitFilter();
+		
+		for (Class<? extends CommitFilter> filterClass : filterClasses) {
+			CommitFilter filterInstance = filterClass.newInstance();
+			compositeFilter.addFilter(filterInstance);
+		}
+		
+		return compositeFilter;
+	}
+
+}
