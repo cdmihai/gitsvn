5f83fe89e2d1d1342cd97008e99d13b1a4dcc65e
Caius Brindescu <caius.brindescu@gmail.com>
2012-10-15 15:24:28 -0500

    Simple dumb main
    This is where the crawler should be called for.
    
    You initialize it with the filters you like, and let it crawl away!
    Sounds easy,
    doesn't it?

---
+++ infra/src/edu/illinois/gitsvn/infra/Main.java
@@ -0,0 +1,19 @@
+package edu.illinois.gitsvn.infra;
+
+
+
+public class Main {
+
+	/**
+	 * Starts the analysis.
+	 * 
+	 * @param args
+	 *            a list of repositories to crawl.
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		for (String repoLoc : args) {
+		}
+	}
+
+}
