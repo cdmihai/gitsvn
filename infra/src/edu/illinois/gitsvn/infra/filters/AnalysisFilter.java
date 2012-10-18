e008898e04625756a1fbedb89599b1793cf3de4d
Mihai Codoban <codo@illinois.edu>
2012-10-18 14:31:17 -0500

    comments

--- infra/src/edu/illinois/gitsvn/infra/filters/AnalysisFilter.java
+++ infra/src/edu/illinois/gitsvn/infra/filters/AnalysisFilter.java
@@ -2,5 +2,10 @@ package edu.illinois.gitsvn.infra.filters;
 
 import org.gitective.core.filter.commit.CommitFilter;
 
+/**
+ *	Represents an Analysis over the commits. (the commit analysis visitor).
+ *	Will most likely end up as a template.
+ * @author mihai
+ */
 public abstract class AnalysisFilter extends CommitFilter implements AnalysisLifecycle{
 }
