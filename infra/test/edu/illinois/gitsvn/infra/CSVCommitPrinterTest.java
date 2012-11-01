34729fb12e72c1a218f0cd44aaeb24e318e2f7da
Mihai Codoban <codo@illinois.edu>
2012-11-01 15:11:20 -0500

    Simplified test to tell order of commits

--- infra/test/edu/illinois/gitsvn/infra/CSVCommitPrinterTest.java
+++ infra/test/edu/illinois/gitsvn/infra/CSVCommitPrinterTest.java
@@ -11,29 +11,30 @@ import edu.illinois.gitsvn.infra.filters.CSVCommitPrinter;
 import edu.illinois.gitsvn.infra.util.CSVWriter;
 
 public class CSVCommitPrinterTest extends GitTestCase {
-	
+
 	@Test
 	public void testSimpleRepo() throws Exception {
-		add("a1", "c1");
-		add("a2", "c2\nmumu");
-		add("a3", "c3");
-		add("a4", "c4");
-		
+		add("a1", "test\ntest\ntest");
+
+		add("a1", "test\ntest1\ntest1");
+
+		add("a1", "test\ntest1\ntest2");
+
 		RepositoryCrawler c = new RepositoryCrawler();
-		
+
 		c.addFilter(CSVCommitPrinter.class);
 		List<AnalysisFilter> filters = c.crawlRepo(testRepo.getAbsolutePath());
-		
+
 		CSVCommitPrinter csvfilter = (CSVCommitPrinter) filters.get(0);
-		
+
 		CSVWriter csvWriter = csvfilter.getCSVWriter();
-		
-		assertEquals(Arrays.asList(new String[]{"id", "time", "lines"}), csvWriter.getHeader());
-		
+
+		assertEquals(Arrays.asList(new String[] { "id", "time", "lines" }), csvWriter.getHeader());
+
 		List<List<String>> rows = csvWriter.getRows();
-		
-		assertEquals(4, rows.size());
-		
+
+		assertEquals(3, rows.size());
+
 		System.out.println(csvWriter.getCSVString());
 	}
 }
