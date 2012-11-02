ec905c99441aff84c8ec4db028c60cf6ad77f48a
Mihai Codoban <codo@illinois.edu>
2012-11-02 16:04:31 -0500

    Added an filter that, in theory, tells us if the commit came from SVN or GIT

---
+++ infra/src/edu/illinois/gitsvn/infra/filters/SVNCommitDetectorFilter.java
@@ -0,0 +1,58 @@
+package edu.illinois.gitsvn.infra.filters;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.gitective.core.filter.commit.CommitMessageFindFilter;
+
+/**
+ * Detects whether a commit was imported from a previous SCM such as SVN
+ * @author mihai
+ *
+ */
+
+public class SVNCommitDetectorFilter extends CommitMessageFindFilter{
+
+	public static String SVN = "SVN";
+	public static String GIT = "GIT";
+	
+	private static String pattern = "SVN";
+	
+	private String mode;
+	
+	
+	public SVNCommitDetectorFilter() {
+		
+		super(pattern, Pattern.MULTILINE);
+	}
+	
+	@Override
+	public RevFilter clone() {
+		return new SVNCommitDetectorFilter();
+	}
+	
+	@Override
+	public boolean include(RevWalk walker, RevCommit commit) throws IOException {
+		boolean lastStatus = super.include(walker, commit);
+		
+		if(lastStatus)
+			mode = SVN;
+		else
+			mode = GIT;
+		
+		return lastStatus;
+	}
+	
+	/**
+	 * Returns SVN if this commit was from SVN.
+	 * Returns GIT otherwise. 
+	 * @return
+	 */
+	public String getMode(){
+		return mode;
+	}
+
+}
