1c4c1fd0b5a6839cb47f89b73f45419ffd5c4964
Mihai Codoban <codo@illinois.edu>
2012-11-14 20:17:01 -0600

    Wrote failing test for the renamer.Shows we may have to compute renamings :(

--- infra/test/edu/illinois/gitsvn/infra/AllDeletionDiffFilterTest.java
+++ infra/test/edu/illinois/gitsvn/infra/AllDeletionDiffFilterTest.java
@@ -10,43 +10,82 @@ import java.nio.file.attribute.BasicFileAttributes;
 import org.gitective.core.CommitFinder;
 import org.gitective.core.filter.commit.AndCommitFilter;
 import org.gitective.core.filter.commit.CommitCountFilter;
+import org.gitective.core.filter.commit.CommitFilter;
 import org.gitective.tests.GitTestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.illinois.gitsvn.infra.filters.blacklister.AllDeletionDiffFilter;
 
 public class AllDeletionDiffFilterTest extends GitTestCase {
 
-	@Test
-	public void testFilterEffect() throws Exception {
-		runTest(true);
-	}
-	
-	@Test
-	public void testnoFilterInterference() throws Exception {
-		runTest(false);
+	private static final int NOTHING = 0;
+	private static final int DELETE = 1;
+	private static final int RENAME = 2;
+
+	private AllDeletionDiffFilter deleteFilter;
+	private CommitCountFilter countFilter;
+	private AndCommitFilter deleteCountAndFilter;
+	private CommitFinder finder;
+	private AndCommitFilter renameCountAndFilter;
+	private AllDeletionDiffFilter renameFilter;
+
+	@Before
+	public void setup() {
+		deleteFilter = AllDeletionDiffFilter.getDeleteDiffFilter();
+		renameFilter = AllDeletionDiffFilter.getRenameDiffFilter();
+		countFilter = new CommitCountFilter();
+
+		deleteCountAndFilter = new AndCommitFilter();
+		deleteCountAndFilter.add(deleteFilter, countFilter);
+
+		renameCountAndFilter = new AndCommitFilter();
+		renameCountAndFilter.add(renameFilter, countFilter);
+
+		finder = new CommitFinder(testRepo);
 	}
 
-	public void runTest(boolean includeDelete) throws Exception {
+	@Test
+	public void testDeleteFilterEffect() throws Exception {
+		runTest(DELETE, deleteCountAndFilter);
+	}
+
+	@Test
+	public void testnoDeleteFilterInterference() throws Exception {
+		runTest(NOTHING, deleteCountAndFilter);
+	}
+
+	@Test
+	public void testRenameFilterEffect() throws Exception {
+		runTest(RENAME, renameCountAndFilter);
+	}
+
+	@Test
+	public void testnoRenameFilterInterference() throws Exception {
+		runTest(NOTHING, renameCountAndFilter);
+	}
+
+	public void runTest(int repoAction, CommitFilter blackListerFilter) throws Exception {
 		int expectedCommitCount = 3;
 		int expectedFilteredCommitCount = 3;
-		
+
 		add("file/a.txt", "mumu", "c1");
 		add("file/a1.txt", "mumu", "c2");
 		add("file/a2.txt", "mumu", "c3");
 
-		if (includeDelete) {
+		switch (repoAction) {
+		case DELETE:
 			delete("file");
 			expectedCommitCount = 4;
+			break;
+
+		case RENAME:
+			mv("file/a.txt", "file/rename.txt", "c4");
+
+			expectedCommitCount = 4;
+
+			// printRepo();
 		}
-
-		AllDeletionDiffFilter deleteFilter = AllDeletionDiffFilter.getDeleteDiffFilter();
-		CommitCountFilter countFilter = new CommitCountFilter();
-
-		AndCommitFilter andFilter = new AndCommitFilter();
-		andFilter.add(deleteFilter, countFilter);
-
-		CommitFinder finder = new CommitFinder(testRepo);
 
 		finder.setFilter(countFilter);
 		finder.find();
@@ -54,7 +93,7 @@ public class AllDeletionDiffFilterTest extends GitTestCase {
 
 		countFilter.reset();
 
-		finder.setFilter(andFilter);
+		finder.setFilter(blackListerFilter);
 		finder.find();
 		assertEquals(expectedFilteredCommitCount, countFilter.getCount());
 	}
