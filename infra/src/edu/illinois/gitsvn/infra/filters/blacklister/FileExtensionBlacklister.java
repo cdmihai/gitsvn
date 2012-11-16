d36fbd59931aa8d8792165f5038d20a96ac3f99b
Mihai Codoban <codo@illinois.edu>
2012-11-15 21:28:55 -0600

    Finished the source code blacklister and its test

--- infra/src/edu/illinois/gitsvn/infra/filters/blacklister/FileExtensionBlacklister.java
+++ infra/src/edu/illinois/gitsvn/infra/filters/blacklister/FileExtensionBlacklister.java
@@ -1,6 +1,8 @@
 package edu.illinois.gitsvn.infra.filters.blacklister;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
@@ -8,8 +10,24 @@ import org.eclipse.jgit.treewalk.TreeWalk;
 import org.gitective.core.filter.tree.BaseTreeFilter;
 
 public final class FileExtensionBlacklister extends BaseTreeFilter {
+	
+	private static final List<String> allowedExtensions = Arrays.asList("java", "py", "c", "cpp");
+	
 	@Override
 	public boolean include(TreeWalk walker) throws MissingObjectException, IncorrectObjectTypeException, IOException {
-		return true;
+		if (walker.isSubtree()){
+			return true;
+		}
+		
+		String name = walker.getNameString();
+		final int extensionStart = name.lastIndexOf('.') + 1;
+		
+		// Ignore names that don't contain a '.' or end with a '.'
+		if (extensionStart == 0 || extensionStart == name.length())
+			return false;
+		
+		name = name.substring(extensionStart);
+		
+		return allowedExtensions.contains(name);
 	}
 }
