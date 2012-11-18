d74eb4ed5fd79489d439cc675040ed29eeda906c
Caius Brindescu <caius.brindescu@gmail.com>
2012-11-18 17:51:09 -0600

    Added a configuration class
    This is usefull for testing purposes.

---
+++ infra/test/edu/illinois/gitsvn/infra/ConfigurationUtil.java
@@ -0,0 +1,27 @@
+package edu.illinois.gitsvn.infra;
+
+import edu.illinois.gitsvn.infra.collectors.AllLineNumberFilter;
+import edu.illinois.gitsvn.infra.collectors.CSVCommitPrinter;
+import edu.illinois.gitsvn.infra.collectors.CutofDetectorFilter;
+import edu.illinois.gitsvn.infra.collectors.JavaLineNumberFilter;
+import edu.illinois.gitsvn.infra.filters.AnalysisFilter;
+import edu.illinois.gitsvn.infra.filters.blacklister.FileOperationBlacklister;
+
+public class ConfigurationUtil {
+
+	public static PipelineCommitFilter configureAnalysis() {
+		PipelineCommitFilter analysisFilter = new PipelineCommitFilter();
+		
+		analysisFilter.addFilter(FileOperationBlacklister.getDeleteDiffFilter());
+		analysisFilter.addFilter(FileOperationBlacklister.getRenameDiffFilter());
+		
+		analysisFilter.addDataCollector(new AllLineNumberFilter());
+		analysisFilter.addDataCollector(new JavaLineNumberFilter());
+		analysisFilter.addDataCollector(new CutofDetectorFilter());
+		
+		AnalysisFilter agregator = new CSVCommitPrinter(analysisFilter.getAllCollectors());
+		analysisFilter.setDataAgregator(agregator);
+		return analysisFilter;
+	}
+
+}
