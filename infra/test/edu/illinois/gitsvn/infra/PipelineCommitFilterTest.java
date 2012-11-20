e7c795945ae7938c59364df4833624e5567430b4
Caius Brindescu <caius.brindescu@gmail.com>
2012-11-20 17:30:41 -0600

    A simple test for the Pipeline filter
    I wrote it to show a bug

---
+++ infra/test/edu/illinois/gitsvn/infra/PipelineCommitFilterTest.java
@@ -0,0 +1,35 @@
+package edu.illinois.gitsvn.infra;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.StopWalkException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.gitective.core.filter.commit.CommitFilter;
+import org.gitective.tests.GitTestCase;
+import org.junit.Test;
+
+public class PipelineCommitFilterTest extends GitTestCase {
+
+	@Test
+	public void testFiltersExclusion() throws Exception {
+		RevCommit cmit = add("test.txt","content","msg");
+		PipelineCommitFilter filter = new PipelineCommitFilter();
+		filter.addFilter(new CommitFilter() {
+			
+			@Override
+			public boolean include(RevWalk walker, RevCommit cmit)
+					throws StopWalkException, MissingObjectException,
+					IncorrectObjectTypeException, IOException {
+				if (cmit.getShortMessage().equals("msg"))
+					return false;
+				
+				return true;
+			}
+		});
+		
+		assertFalse(filter.include(null, cmit));
+	}
+}
