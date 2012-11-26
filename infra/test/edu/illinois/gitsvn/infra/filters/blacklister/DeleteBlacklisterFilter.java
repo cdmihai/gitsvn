7feb393d4cba3a54b9f53669f8ec868581018bee
Caius Brindescu <caius.brindescu@gmail.com>
2012-11-26 10:27:14 -0600

    Added a new test for the delete blacklist filter

--- infra/test/edu/illinois/gitsvn/infra/filters/blacklister/DeleteBlacklisterFilter.java
+++ infra/test/edu/illinois/gitsvn/infra/filters/blacklister/DeleteBlacklisterFilter.java
@@ -1,5 +1,8 @@
 package edu.illinois.gitsvn.infra.filters.blacklister;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
@@ -29,7 +32,8 @@ public class DeleteBlacklisterFilter extends GitTestCase {
 
 	@Test
 	public void testDeleteSomeContents() throws Exception {
-		RevCommit commitToFilter = add("test.txt", "line 1\nline 2\nline 3\n", "");
+		RevCommit commitToFilter = add("test.txt", "line 1\nline 2\nline 3\n",
+				"");
 
 		boolean actual = deleteDiffFilter.include(revWalk, commitToFilter);
 		assertTrue(actual);
@@ -38,8 +42,32 @@ public class DeleteBlacklisterFilter extends GitTestCase {
 	@Test
 	public void testDeleteFile() throws Exception {
 		RevCommit commitToFilter = delete("test.txt");
-		
+
 		boolean actual = deleteDiffFilter.include(revWalk, commitToFilter);
 		assertFalse(actual);
+	}
+
+	@Test
+	public void testDeleteOneFileChangeAnother() throws Exception {
+		String deletedFile = "test.txt";
+		String modifiedFile = "modified.txt";
+		
+		add(deletedFile, "some content", "first");
+		add(modifiedFile, "some content", "second");
+		
+		Git git = Git.open(testRepo);
+		git.rm().addFilepattern(deletedFile).call();
+		File file = new File(testRepo.getParent(), modifiedFile);
+		PrintWriter writer = new PrintWriter(file);
+		writer.write("Some other content");
+		writer.close();
+		git.add().addFilepattern(modifiedFile).call();
+		RevCommit commit = git.commit().setOnly(deletedFile)
+				.setOnly(modifiedFile).setMessage("third").setAuthor(author)
+				.setCommitter(committer).call();
+				
+		boolean actual = deleteDiffFilter.include(revWalk, commit);
+		assertTrue(actual);
+
 	}
 }
