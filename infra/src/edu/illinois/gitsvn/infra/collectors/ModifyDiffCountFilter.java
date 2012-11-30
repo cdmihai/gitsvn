81c28cdf57dd7ec514c8d5f696e63f7a3c157b48
Mihai Codoban <codo@illinois.edu>
2012-11-29 19:59:59 -0600

    Extended DiffCountFilter so as to make it accept only Modify Diffs (not add, remove, rename and copy).
    This is not a good solution because other filters that want the same
    functionality but extend the Diff filter hierarchy in another place have
    to clone this logic.
    The correct solution should be to implement a TreeFilter and use the
    same strategy like the java source filters.

---
+++ infra/src/edu/illinois/gitsvn/infra/collectors/ModifyDiffCountFilter.java
@@ -0,0 +1,16 @@
+package edu.illinois.gitsvn.infra.collectors;
+
+import java.util.Collection;
+
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.diff.Edit;
+import org.gitective.core.filter.commit.DiffCountFilter;
+
+public class ModifyDiffCountFilter extends DiffCountFilter {
+
+	@Override
+	protected boolean acceptDiff(DiffEntry diff, Collection<Edit> edits) {
+		return diff.getChangeType().equals(ChangeType.MODIFY) ? true : false;
+	}
+}
