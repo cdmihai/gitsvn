bf28c43f0e66a4bdb92b31b55773ea9759048fc6
cdmihai <codo@illinois.edu>
2012-12-13 14:02:13 -0600

    Updated the ModifyDiffCountFilter to also support the filtering of edits from each diff.
    It does so by depending on EditFilters which provide different filtering logic.

--- infra/src/edu/illinois/gitsvn/infra/collectors/diff/ModifyDiffCountFilter.java
+++ infra/src/edu/illinois/gitsvn/infra/collectors/diff/ModifyDiffCountFilter.java
@@ -1,20 +1,73 @@
 package edu.illinois.gitsvn.infra.collectors.diff;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.lib.Repository;
+import org.gitective.core.filter.commit.CommitFilter;
 import org.gitective.core.filter.commit.DiffCountFilter;
 
+import edu.illinois.gitsvn.infra.collectors.diff.editfilter.CommentEditFilter;
+import edu.illinois.gitsvn.infra.collectors.diff.editfilter.EditFilter;
+import edu.illinois.gitsvn.infra.collectors.diff.editfilter.FormatEditFilter;
+
+/**
+ * DiffCountFilter that accepts only diffs of type Modify. Also, it can be
+ * configure with a list of {@link EditFilter} that further filters diffs' edits (such as formatting or comment edit)
+ * 
+ * @author mihai
+ * 
+ */
 public class ModifyDiffCountFilter extends DiffCountFilter {
 
-	public ModifyDiffCountFilter(boolean enableRenamings) {
-		super(enableRenamings);
+	private List<EditFilter> editFilters;
+
+	public ModifyDiffCountFilter(List<EditFilter> editFilters) {
+		super(true);
+		this.editFilters = editFilters;
 	}
-	
+
+	public ModifyDiffCountFilter(boolean enableRenamings) {
+		this(new ArrayList<EditFilter>());
+	}
+
+	@Override
+	public CommitFilter setRepository(Repository repository) {
+		for (EditFilter editFilter : editFilters) {
+			editFilter.setRepository(repository);
+		}
+
+		return super.setRepository(repository);
+	}
+
 	@Override
 	protected boolean acceptDiff(DiffEntry diff, Collection<Edit> edits) {
 		return diff.getChangeType().equals(ChangeType.MODIFY) ? true : false;
+	}
+
+	@Override
+	protected Collection<Edit> filterEdits(DiffEntry diff, Collection<Edit> edits) {
+		for (EditFilter editFilter : editFilters) {
+			edits = editFilter.filterEdits(diff, edits);
+		}
+
+		return edits;
+	}
+
+	public static ModifyDiffCountFilter getWithAllEditFilters() {
+		return new ModifyDiffCountFilter(Arrays.asList(new EditFilter[] { getCommentEditFilter(), getFormatEditFilter() }));
+	}
+
+	public static EditFilter getFormatEditFilter() {
+		return new FormatEditFilter();
+	}
+
+	public static EditFilter getCommentEditFilter() {
+		return new CommentEditFilter();
 	}
 }
