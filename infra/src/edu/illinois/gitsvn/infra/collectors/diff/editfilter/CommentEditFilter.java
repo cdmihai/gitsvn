88ed3b9169be29bde74f0cd19dc39dba45df029f
cdmihai <codo@illinois.edu>
2012-12-13 21:17:41 -0600

    implemented the EditFilter that ignores some edits in comments

--- infra/src/edu/illinois/gitsvn/infra/collectors/diff/editfilter/CommentEditFilter.java
+++ infra/src/edu/illinois/gitsvn/infra/collectors/diff/editfilter/CommentEditFilter.java
@@ -1,13 +1,47 @@
 package edu.illinois.gitsvn.infra.collectors.diff.editfilter;
 
+import java.util.Scanner;
+
 import org.eclipse.jgit.diff.Edit;
-import org.eclipse.jgit.lib.Repository;
 
 public class CommentEditFilter extends EditFilter {
 
 	@Override
 	protected boolean shouldInclude(String oldContent, String newContent, Edit edit) {
-		// TODO Auto-generated method stub
+		boolean allLinesHaveHeader = linesStartWithHeader(oldContent, "*", "//", "/*", "/**") && linesStartWithHeader(newContent, "*", "//", "/*", "/**");
+		
+		return !allLinesHaveHeader;
+	}
+
+	/**
+	 * Checks that each line from the content parameter starts with either one of the headers
+	 * @param content
+	 * @param headers
+	 * @return
+	 */
+	private boolean linesStartWithHeader(String content, String ... headers) {
+		if(content.equals(""))
+			return true;
+		
+		try(Scanner s = new Scanner(content)) {
+			while(s.hasNextLine()){
+				String line = s.nextLine().trim();
+				
+				if (!lineHasAtLeastOneHeader(line, headers))
+					return false;
+			}
+		}
+		
+		return true;
+	}
+
+	private boolean lineHasAtLeastOneHeader(String line, String... headers) {
+		
+		for (String header : headers) {
+			if (line.startsWith(header))
+				return true;
+		}
+		
 		return false;
 	}
 
