0f12fb718e08cfa73f5795a8ae3e7c1ab7a54833
cdmihai <codo@illinois.edu>
2013-06-13 18:34:56 -0500

    created analysis launcher that runs for all git repos inside a parent directory

---
+++ infra/src/edu/illinois/gitsvn/infra/BatchAnalysisLauncher.java
@@ -0,0 +1,24 @@
+package edu.illinois.gitsvn.infra;
+
+import java.io.File;
+import java.util.List;
+
+import edu.illinois.gitsvn.analysis.GenericAnalysis;
+
+public class BatchAnalysisLauncher extends AnalysisLauncher {
+	
+	public static void main(String[] args) throws Exception {
+		new BatchAnalysisLauncher().run();
+	}
+	
+	@Override
+	protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
+		String reposPath = "../../gitProjects";
+		
+		File parent = new File(reposPath);
+		
+		for (File f : parent.listFiles()) {
+			configurations.add(new GenericAnalysis(f.getAbsolutePath(), f.getName()));
+		}
+	}
+}
