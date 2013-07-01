7324c19d98057a90c9f52150e5da7590fdbacf08
cdmihai <codo@illinois.edu>
2013-07-01 16:14:36 -0500

    reworked scripts to: optionally provide git repo name; download all repositories in different folders

---
+++ infra/downloadRepos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./repoget.sh repositories/svnToGitRepos.txt ../../svnToGitRepos
+./repoget.sh repositories/pureGitRepos.txt ../../pureGitRepos
+./repoget.sh repositories/personalRepos.txt ../../personalSvnToGitRepos
