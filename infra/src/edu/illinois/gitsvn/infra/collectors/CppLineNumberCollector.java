d95adda4d497bf9d3e88c5f7690617443d63a49d
cdmihai <codo@illinois.edu>
2013-07-01 18:03:02 -0500

    removed unused imports

--- infra/src/edu/illinois/gitsvn/infra/collectors/CppLineNumberCollector.java
+++ infra/src/edu/illinois/gitsvn/infra/collectors/CppLineNumberCollector.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
-import org.eclipse.jgit.errors.StopWalkException;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.TreeWalk;
