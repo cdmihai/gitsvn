f969e80d979da24ff68ee8897f22e274ff492131
Caius Brindescu <brind@illinois.edu>
2013-07-19 16:01:33 -0500

    Parameterized the NonSourceFileBlacklister
    Since we are using it for a number of languages, it makes
    sense to parameterize it so it's easily reusable. Now, you
    have to give it a list of extension you consider to be source
    files.
    
    To make life easier I created a few predefined lists for Java
    source files and "all" (for now Java, C, C++ and Python).
    More can be added if need may be.

--- infra/src/edu/illinois/gitsvn/infra/filters/blacklister/NonSourceCodeFileExtensionBlacklister.java
+++ infra/src/edu/illinois/gitsvn/infra/filters/blacklister/NonSourceCodeFileExtensionBlacklister.java
@@ -12,7 +12,14 @@ import org.gitective.core.filter.tree.BaseTreeFilter;
 //TODO make this into a configurable extension blocker and use factory methods for java blocker, source code blocker etc
 public final class NonSourceCodeFileExtensionBlacklister extends BaseTreeFilter {
 	
-	private static final List<String> allowedExtensions = Arrays.asList("java", "js", "c", "cpp", "h", "ino", "py");
+
+	public static final List<String> ALL_SOURCE_EXTENSIONS = Arrays.asList("java", "js", "c", "cpp", "h", "ino", "py");
+	public static final List<String> JAVA_EXTENSIONS = Arrays.asList("java");
+	private List<String> sourceExtensions;
+	
+	public NonSourceCodeFileExtensionBlacklister(List<String> sourceExtensions) {
+		this.sourceExtensions = sourceExtensions;
+	}
 	
 	@Override
 	public boolean include(TreeWalk walker) throws MissingObjectException, IncorrectObjectTypeException, IOException {
@@ -29,6 +36,6 @@ public final class NonSourceCodeFileExtensionBlacklister extends BaseTreeFilter 
 		
 		name = name.substring(extensionStart);
 		
-		return allowedExtensions.contains(name);
+		return sourceExtensions.contains(name);
 	}
 }
