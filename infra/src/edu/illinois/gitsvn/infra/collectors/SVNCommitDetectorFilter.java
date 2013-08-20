f2b0623fd120dfd6ceaac096639c579ddbd265f2
cdmihai <codo@illinois.edu>
2013-08-20 11:49:18 -0700

    class comment added

--- infra/src/edu/illinois/gitsvn/infra/collectors/SVNCommitDetectorFilter.java
+++ infra/src/edu/illinois/gitsvn/infra/collectors/SVNCommitDetectorFilter.java
@@ -8,48 +8,50 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.gitective.core.filter.commit.CommitMessageFindFilter;
 
-
 /**
- * Detects whether a commit was imported from a previous SCM such as SVN
+ * Detects whether a commit was imported from a previous SCM such as SVN. It
+ * does so by searching for a special string that is concatenated to the commit
+ * by the svn2git conversion tool
+ * 
  * @author mihai
- *
+ * 
  */
 
 public class SVNCommitDetectorFilter extends CommitMessageFindFilter implements AbstractSVNDetector {
 
 	private static String pattern = "git-svn-id";
-	
+
 	private String mode;
-	
+
 	public SVNCommitDetectorFilter() {
 		super(pattern, Pattern.MULTILINE);
 	}
-	
+
 	@Override
 	public RevFilter clone() {
 		return new SVNCommitDetectorFilter();
 	}
-	
+
 	@Override
 	public boolean include(RevWalk walker, RevCommit commit) throws IOException {
 		boolean lastStatus = super.include(walker, commit);
-		
-		if(lastStatus)
+
+		if (lastStatus)
 			mode = SVN;
 		else
 			mode = GIT;
-		
+
 		return lastStatus;
 	}
-	
+
 	@Override
 	public String name() {
 		return "SCM";
 	}
 
 	/**
-	 * Returns SVN if this commit was from SVN.
-	 * Returns GIT otherwise. 
+	 * Returns SVN if this commit was from SVN. Returns GIT otherwise.
+	 * 
 	 * @return
 	 */
 	@Override
