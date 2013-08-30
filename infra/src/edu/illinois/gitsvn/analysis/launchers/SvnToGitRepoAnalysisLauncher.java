22117305d74bf6dbdd96d6ea09af51ec2eb9fbb9
cdmihai <codo@illinois.edu>
2013-08-30 13:16:32 -0700

    new configuration

--- infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
@@ -13,42 +13,42 @@ public class SvnToGitRepoAnalysisLauncher extends AnalysisLauncher {
 	@Override
 	protected void populateWithConfigurations(List<AnalysisConfiguration> configurations) {
 //		configurations.add(new CutoffGenericAnalysis("../../projects/junit", "JUnit", 1231818542));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/junit", "JUnit", 1231818542));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/Arduino", "Arduino", 1284741802));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/cyclopsgroup", "CyclopsGroup", 1295380000));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.core", "JDTCore", 1316790694));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.debug", "JDTDebug", 1316538989));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.ui", "JDTUI", 1316608366));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform", "EclipsePlatform", 1317272400));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.common", "EclipsePlatformCommon", 1318741200));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.debug", "EclipsePlatformDebug", 1316408400));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.team", "EclipsePlatformTeam", 1316581200));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.text", "EclipsePlatformText", 1316149200));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/FFmpeg", "FFmpeg", 1295258573));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/libreoffice", "LibreOffice", 1282256340));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/MPS", "MPS", 1277726186));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/prototype", "prototype", 1205935492));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/thymeleaf", "Thymeleaf", 1337530081));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/upm-swing/", "UPM", 1287344636));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/astrid/", "astrid", 1273138392));
-		
-
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/clojure/", "clojure", 1229436656));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/spring-framework/", "spring-framework", 1266882776));
-		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/yuicompressor/", "yuicompressor", 1232056073));
-
-
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/cassandra", "cassandra"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/Essentials", "Essentials"));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/junit", "JUnit", 1231818542));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/Arduino", "Arduino", 1284741802));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/cyclopsgroup", "CyclopsGroup", 1295380000));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.core", "JDTCore", 1316790694));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.debug", "JDTDebug", 1316538989));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.jdt.ui", "JDTUI", 1316608366));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform", "EclipsePlatform", 1317272400));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.common", "EclipsePlatformCommon", 1318741200));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.debug", "EclipsePlatformDebug", 1316408400));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.team", "EclipsePlatformTeam", 1316581200));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/eclipse.platform.text", "EclipsePlatformText", 1316149200));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/FFmpeg", "FFmpeg", 1295258573));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/libreoffice", "LibreOffice", 1282256340));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/MPS", "MPS", 1277726186));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/prototype", "prototype", 1205935492));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/thymeleaf", "Thymeleaf", 1337530081));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/upm-swing/", "UPM", 1287344636));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/astrid/", "astrid", 1273138392));
+//		
+//
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/clojure/", "clojure", 1229436656));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/spring-framework/", "spring-framework", 1266882776));
+//		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/yuicompressor/", "yuicompressor", 1232056073));
+//
+//
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/cassandra", "cassandra"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/Essentials", "Essentials"));
 		configurations.add(new CutoffGenericAnalysis("../../svnToGitRepos/k-9", "k-9", 1296960339));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/liferay-plugins", "liferay-plugins"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/liferay-portal", "liferay-portal"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/twitter4j", "twitter4j"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/gradle", "gradle"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/hibernate-orm", "hibernate-orm"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/jenkins", "jenkins"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/grails-core", "grails-core"));
-		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/groovy-core", "groovy-core"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/liferay-plugins", "liferay-plugins"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/liferay-portal", "liferay-portal"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/twitter4j", "twitter4j"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/gradle", "gradle"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/hibernate-orm", "hibernate-orm"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/jenkins", "jenkins"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/grails-core", "grails-core"));
+//		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/groovy-core", "groovy-core"));
 	}
 
 	public static void main(String[] args) throws Exception {
