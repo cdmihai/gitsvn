2a8f67785ba3f16810508c54e948f2afce59fddc
cdmihai <codo@illinois.edu>
2013-09-08 14:05:46 -0700

    prepped for extra repos

--- infra/src/edu/illinois/gitsvn/analysis/launchers/PureGitLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/PureGitLauncher.java
@@ -4,8 +4,8 @@ import edu.illinois.gitsvn.infra.DirectoryAnalysisLauncher;
 
 public class PureGitLauncher {
 	public static void main(String[] args) throws Exception {
-		DirectoryAnalysisLauncher launcher = new DirectoryAnalysisLauncher("../../pureGitRepos");
-		launcher.setResultsPath("../../pureGitResults");
+		DirectoryAnalysisLauncher launcher = new DirectoryAnalysisLauncher("../../extraPureGitRepos");
+		launcher.setResultsPath("../../extraPureGitResults");
 		launcher.run();
 	}
 }
