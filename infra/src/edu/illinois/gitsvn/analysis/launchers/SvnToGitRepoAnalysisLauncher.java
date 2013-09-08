1b89805628fd166ec8e338bd88cf78fb13e544a3
altern <sergey.shmarkatyuk@gmail.com>
2013-09-08 15:22:36 -0500

    Added wicket repo to the list of svntogit repos for cutoff analysis

--- infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/SvnToGitRepoAnalysisLauncher.java
@@ -50,6 +50,7 @@ public class SvnToGitRepoAnalysisLauncher extends AnalysisLauncher {
 		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/grails-core", "grails-core"));
 		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/groovy-core", "groovy-core"));
 		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/couchdb", "couchdb"));
+		configurations.add(new GitSvnCutoffAnalysis("../../svnToGitRepos/wicket", "wicket"));
 	}
 
 	public static void main(String[] args) throws Exception {
