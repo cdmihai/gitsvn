9700cdabe60b60690896ac1cab2866f1e8f75ed5
cdmihai <codo@illinois.edu>
2013-09-10 13:43:23 -0700

    prepped for launch

--- infra/src/edu/illinois/gitsvn/analysis/launchers/PureGitLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/PureGitLauncher.java
@@ -5,7 +5,7 @@ import edu.illinois.gitsvn.infra.DirectoryAnalysisLauncher;
 public class PureGitLauncher {
 	public static void main(String[] args) throws Exception {
 		DirectoryAnalysisLauncher launcher = new DirectoryAnalysisLauncher("../../pureGitRepos");
-		launcher.setResultsPath("../../pureGitResults");
+		launcher.setResultsPath("../../gitLoc");
 		launcher.run();
 	}
 }
--- infra/src/edu/illinois/gitsvn/analysis/launchers/PureSvnLauncher.java
+++ infra/src/edu/illinois/gitsvn/analysis/launchers/PureSvnLauncher.java
@@ -4,8 +4,8 @@ import edu.illinois.gitsvn.infra.DirectoryAnalysisLauncher;
 
 public class PureSvnLauncher {
 	public static void main(String[] args) throws Exception {
-		DirectoryAnalysisLauncher launcher = new DirectoryAnalysisLauncher("../../pureSvnRepos");
-		launcher.setResultsPath("../../pureSvnResults");
+		DirectoryAnalysisLauncher launcher = new DirectoryAnalysisLauncher("/home/codo/repos/svnRepos");
+		launcher.setResultsPath("../../svnLoc");
 		launcher.run();
 	}
 }
